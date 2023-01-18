409f72fa2cb0f2ef8b3372003a9811f206d16b08
JaeJinByun <byunjin11@naver.com>
2023-01-18 23:10:00 +0900

    🐛 [fix] 버그 수정 / 설정 정보 수정

--- multiWallet/main.go
+++ multiWallet/main.go
@@ -61,7 +61,7 @@ func init() {
 	} else if userc = mod.Client.Database(cf.DB.MultiWalletDatabase).Collection(cf.DB.UserInfoColl); err != nil {
 		panic(err)
 		/* 서비스 초기화 */
-	} else if walletc = mod.Client.Database(cf.DB.MultiWalletDatabase).Collection(cf.DB.WalletColl); err != nil {
+	} else if walletc = mod.Client.Database(cf.DB.MultiWalletDatabase).Collection(cf.DB.WalletInfoColl); err != nil {
 		panic(err)
 	} else if us, err = services.NewUserService(userc, context.TODO()); err != nil {
 		panic(err)
